34aeddfa4375971bfc5cc4f39236a1d3fae55179
killer-bee07 <tedharington@gmail.com>
2024-09-15 23:25:10 +0100

    n

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 // import { IonIcon } from "react-ion-icon"; // Assurez-vous d'importer cela si vous utilisez cette bibliothèque
-import logo from "../assets/Logo.png";
+import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
 
 const Header = () => {
@@ -96,12 +96,12 @@ const Header = () => {
           >
             <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
               <li>
-              <Link
-                to="/"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Home
-              </Link>
+                <Link
+                  to="/"
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Home
+                </Link>
               </li>
               <Link
                 to="/products"
